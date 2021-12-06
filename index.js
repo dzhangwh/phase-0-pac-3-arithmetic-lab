e8609d2f484090a9621e4a79f7da157dc135617d
Daniel Zhang <dzhangwh@gmail.com>
2021-12-05 22:02:32 -0500

    Completed

---
+++ index.js
@@ -0,0 +1,36 @@
+function add (a, b) {
+return (a + b);
+}
+
+function subtract (a, b) {
+    return (a - b);
+}
+
+function multiply (a, b){
+    return (a * b);
+}
+
+function divide (a, b) {
+    return (a / b);
+}
+
+function increment (n) {
+    n++
+    return (n);
+}
+
+function decrement (n) {
+    n--
+    return (n);
+}
+
+function makeInt (n, base) {
+    parseInt("9000", 10);
+    base = 10;
+    return (parseInt(n, base));
+}
+
+function preserveDecimal (n) {
+    parseFloat(n);
+    return (parseFloat(n))
+}
